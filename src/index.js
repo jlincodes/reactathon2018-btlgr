8d5741aec609237246d64968376149aa9a33b87b
Julie <julie.lin0812@gmail.com>
2018-03-24 16:38:43 -0700

    Add semi-colons

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { configureStore } from './store/store'
+import { configureStore } from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore()
+  let store = configureStore();
 
   ReactDOM.render(<App />, document.getElementById('root'));
   registerServiceWorker();
-})
-
+});
