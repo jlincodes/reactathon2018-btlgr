3682d60bcc770504390f2696ce45c1b3991b5f29
Tyler Wood <the.tyler.wood@gmail.com>
2018-03-25 10:56:07 -0700

    netlify lambda pushing to hasura

--- src/lambda/eventbrite.js
+++ src/lambda/eventbrite.js
@@ -4,16 +4,44 @@ export function handler(event, context, callback) {
   
   let info;
   
-  console.log(process.env.eventbriteKey);
 
-  axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=${process.env.eventbriteKey}&start_date.keyword=today&sort_by=best&q=heightened_series`)
+  axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=I6KMPRJD2RIBTZ23G2LR&start_date.keyword=today&sort_by=best&q=karaoke`)
     .then(resp => {      
-      info = resp.data.events;
-      console.log(info);
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({msg: info})
+      info = resp.data.events.map(item => {
+        return(
+          {"Name":`${item.name.text}`,
+           "Description":`${item.description.text}`}
+        )
       });
-    })
-  
+      
+      let data = {
+        "type":"insert",
+        "args":{
+          "table":"Events",
+          "objects":info,
+          "returning":["id","Name"]
+        }
+      }
+      
+      let requestOptions = {
+          "method": "POST",
+          "headers": {
+              "Content-Type": "application/json"
+          }
+      }   
+         
+      axios.post(`https://data.annihilate87.hasura-app.io/v1/query`, data, requestOptions).then(resp => {
+        info = resp
+      
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(data)
+        });
+
+      }).catch(err => {
+        console.log('^&$&%^$&^%$&%^$&%^$&^%$&^%$&^%$&^%$&^%$&%^$');
+        console.log(err);
+      })
+    }
+  )
 }
