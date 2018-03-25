0db46e18d3cf23f83cb8d28e3aa1837c4e834cf7
Julie <julie.lin0812@gmail.com>
2018-03-25 01:39:08 -0700

    Create event index and event list item components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React, { Component } from 'react';
 
-import './App.css';
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {loading: false, msg: null};
-  }
-
-  handleClick = (e) => {
-    e.preventDefault();
-
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-  }
-
-  render() {
-    const {loading, msg} = this.state;
-
-    return <p>
-      <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-      <span>{msg}</span>
-    </p>
-  }
-}
+import './app.css';
+import EventIndex from './components/events/event_index';
+import Splash from './components/splash/splash';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LambdaDemo/>
+        <Splash />
+        <EventIndex/>
       </div>
     );
   }
