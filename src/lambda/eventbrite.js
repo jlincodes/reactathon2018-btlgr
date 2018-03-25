4ecb1cc66cba321106d75a9ee451337b4e82da8b
ryan mapa <ryan.m.mapa@gmail.com>
2018-03-25 11:45:57 -0700

    Fix eventbrite

--- src/lambda/eventbrite.js
+++ src/lambda/eventbrite.js
@@ -6,11 +6,8 @@ export function handler(event, context, callback) {
 
   let key = process.env.eventbriteKey ? process.env.eventbriteKey : "I6KMPRJD2RIBTZ23G2LR";
 
-<<<<<<< HEAD
-  axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=${key}&start_date.keyword=today&sort_by=best&q=${event.body}`
-=======
+
   axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=${key}&start_date.keyword=today&sort_by=best&q=${event.body}`)
->>>>>>> 6fa0f0d90313c2918efe71a8362dfa099eb3b6c5
     .then(resp => {
       info = resp.data.events.map(item => {
         return(
@@ -26,18 +23,14 @@ export function handler(event, context, callback) {
           "objects":info,
           "returning":["id","Name"]
         }
-<<<<<<< HEAD
       }
-=======
-      };
->>>>>>> 6fa0f0d90313c2918efe71a8362dfa099eb3b6c5
+
 
       let requestOptions = {
           "method": "POST",
           "headers": {
               "Content-Type": "application/json"
           }
-<<<<<<< HEAD
       }
 
       axios.post(`https://data.annihilate87.hasura-app.io/v1/query`, data, requestOptions).then(resp => {
@@ -54,23 +47,4 @@ export function handler(event, context, callback) {
       })
     }
   )
-=======
-      };
-
-      axios.post(`https://data.annihilate87.hasura-app.io/v1/query`, data, requestOptions)
-        .then(response => {
-          info = response;
-
-          callback(null, {
-            statusCode: 200,
-            body: JSON.stringify(data)
-          });
-
-        })
-        .catch(err => {
-          console.log('^&$&%^$&^%$&%^$&%^$&^%$&^%$&^%$&^%$&^%$&%^$');
-          console.log(err);
-        });
-    });
->>>>>>> 6fa0f0d90313c2918efe71a8362dfa099eb3b6c5
 }
