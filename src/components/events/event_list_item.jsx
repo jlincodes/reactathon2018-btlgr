255a9a09f7a50580343e452e5c995b1ee3ed0c1c
Julie <julie.lin0812@gmail.com>
2018-03-25 14:16:31 -0700

    Removes console logs

--- src/components/events/event_list_item.jsx
+++ src/components/events/event_list_item.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const EventListItem = (props) => {
-  console.log("props", props);
   const eventId = props.event.id;
   const eventName = props.event.Name;
   // const eventDesc = props.event.description.slice(0, 300);
