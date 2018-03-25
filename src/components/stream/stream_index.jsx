c995c64b36d0d0ef7640d20554beb9da40b3db4d
Clay Shaw <Claydshaw@gmail.com>
2018-03-25 10:53:00 -0700

    skeleton

--- src/components/stream/stream_index.jsx
+++ src/components/stream/stream_index.jsx
@@ -3,6 +3,17 @@ import React, { Component } from 'react'
 class StreamIndex extends Component {
   constructor(props){
     super(props)
-    
+    this.state = {
+      streams = []
+    }
+  }
+
+  render() {
+    let { streams } = this.props
+    return (
+      <div className='StreamIndex'>
+        streams
+      </div>
+    )
   }
 }
