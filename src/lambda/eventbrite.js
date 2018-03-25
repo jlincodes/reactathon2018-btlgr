cff8cd24b5b5e25ddeec39fe365309c427af3732
Tyler Wood <the.tyler.wood@gmail.com>
2018-03-25 11:35:06 -0700

    bugfix in lambda

--- src/lambda/eventbrite.js
+++ src/lambda/eventbrite.js
@@ -6,7 +6,7 @@ export function handler(event, context, callback) {
   
   let key = process.env.eventbriteKey ? process.env.eventbriteKey : "I6KMPRJD2RIBTZ23G2LR";
 
-  axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=${key}&start_date.keyword=today&sort_by=best&q=${event.body}`
+  axios.get(`https://www.eventbriteapi.com/v3/events/search/?token=${key}&start_date.keyword=today&sort_by=best&q=${event.body}`)
     .then(resp => {      
       info = resp.data.events.map(item => {
         return(
